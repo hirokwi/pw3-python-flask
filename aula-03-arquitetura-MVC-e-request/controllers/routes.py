bbbfc481444616d141a2d5fe94ce251a04670493
HEITOR HIROKI KUSAKA DE MORAES <heitor.moraes@e239.local>
2026-03-30 11:09:38 -0300

    aula 3 mvc

--- aula-03-arquitetura-MVC-e-request/controllers/routes.py
+++ aula-03-arquitetura-MVC-e-request/controllers/routes.py
@@ -1,52 +1,62 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 
 def init_app(app):
 
-  from flask import render_template
+    listaGames = [
+        {"titulo": "CS-GO", "ano": 2012, "categoria": "FPS online"}
+    ]
 
-
-# criando a função para receber o flask (app)
-def init_app(app):
-    # a partir daqui virão as rotas
-    
     @app.route('/')
-    # Def serve para criar funções no Python
     def home():
         return render_template('index.html')
 
-
     @app.route('/games')
     def games():
-
         titulo = "Warframe"
         ano = 1999
-        categoria = "RPG"    
-        jogadores = ['Eduardo', 'Ana', 'Guilherme', 'Vitor', 'Antonio']
-        
-        game = {
-        "titulo" : "Warframe",
-        "ano" : 1999,
-        "categoria" : "RPG"}
-        
-        return render_template('games.html',
-        titulo = titulo,
-        ano = ano,
-        categoria = categoria,
-        jogadores = jogadores,
-        game = game,)
+        categoria = "RPG"
 
+        jogadores = ['Eduardo', 'Ana', 'Guilherme', 'Vitor', 'Antonio']
+
+        game = {
+            "titulo": "Warframe",
+            "ano": 1999,
+            "categoria": "RPG"
+        }
+
+        return render_template(
+            'games.html',
+            titulo=titulo,
+            ano=ano,
+            categoria=categoria,
+            jogadores=jogadores,
+            game=game
+        )
 
     @app.route('/consoles')
     def consoles():
         nome = "Nintendo Switch"
         lançamento = 1969
         marca = "Nintendo"
-        
+
         consoles = ['PS4', 'Xbox', 'Nintendo Switch', 'PS5', 'PS1']
-        
-        return render_template('consoles.html',
-        nome = nome,
-        lançamento = lançamento,
-        marca = marca,
-        consoles = consoles,)
+
+        return render_template(
+            'consoles.html',
+            nome=nome,
+            lançamento=lançamento,
+            marca=marca,
+            consoles=consoles
+        )
+
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        # verficando se o método da requisição é POSt
+        if request.method == 'POST':
+            # recebendo os dados do formulário e gravando na lista
+            listaGames.append({'titulo' : request.form.get('titulo'), 'ano': request.form.get('ano'), 'categoria' : request.form.get('categoria')})
+            # metodo append() adiciona valores a lista
+            
+            return redirect(url_for('cadgames'))
+        return render_template('cadgames.html', listaGames = listaGames)
